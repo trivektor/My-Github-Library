66226043748eabd8f1a4445009617f9b56e6de21
Tri Vuong <trivektor@gmail.com>
2011-10-16 14:54:12 -0700

    update sync status to user

--- app/assets/javascripts/views/githubSyncView.js
+++ app/assets/javascripts/views/githubSyncView.js
@@ -4,7 +4,7 @@ $(function() {
     el: $("#dashboard"),
     initialize: function() {
       this.setupSelectors();
-      this.collection.bind("add", this.hideSpinner, this);
+      this.collection.bind("add", this.updateSyncStatus, this);
     },
     setupSelectors: function() {
       this.spinner = $("#spinner");
@@ -15,6 +15,10 @@ $(function() {
     syncGithub: function() {
       this.showSpinner();
       this.collection.sync();
+    },
+    updateSyncStatus: function() {
+      this.hideSpinner();
+      alert("Your repositories have been synched");
     },
     hideSpinner: function() {
       this.spinner.hide();
